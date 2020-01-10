209d7995aaf3e2468af658a176eb398016c8d658
Delbert F <7155672+delbertf@users.noreply.github.com>
2020-01-11 00:42:58 +0100

    fix

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,15 @@
-#write your code here
+def echo(arg)
+	arg
+end
+
+def shout(word)
+	word.upcase
+end
+
+def repeat(word, times=2)
+	[word] * times * ' '
+end
+
+def start_of_word(word, start)
+	word[0,start]
+end
