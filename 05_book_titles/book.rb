5808bec93d16bc2f37d357ed7319bb8e1bfde495
Delbert F <7155672+delbertf@users.noreply.github.com>
2020-01-12 22:16:11 +0100

    fix tests 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,9 @@
 class Book
 # write your code here
+	attr_accessor :title
+
+	def title=(value)
+		excluded_words = ["and","over","the","in","of","a","an"]
+		@title = value.split.each_with_index.map { |str, idx| excluded_words.include?(str) && idx > 0 ? str : str.capitalize }.join(" ")
+	end
 end
