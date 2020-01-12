4dda727912e39190a65df0dcbadfec1485cd16e2
Delbert F <7155672+delbertf@users.noreply.github.com>
2020-01-12 21:09:39 +0100

    fix tests

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -13,3 +13,14 @@ end
 def start_of_word(word, start)
 	word[0,start]
 end
+
+def first_word(word)
+	word.split(" ")[0]
+end
+
+def titleize(word)
+	excluded_words = ["and","over","the"]
+	word.split.each_with_index.map { |str, idx| excluded_words.include?(str) && idx > 0 ? str : str.capitalize }.join(" ")
+
+
+end
