9a8f846387c4507689e52d3c136d6587dddc237e
Delbert F <7155672+delbertf@users.noreply.github.com>
2020-01-12 21:59:45 +0100

    fix tests for 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,36 @@
-#write your code here
+def translate(sentence)
+		
+
+	#sentence.split.map { |word| vowels.include?(word[0]) ? word + "ay" :  word[1..-1] + word[0] + "ay"  }.join(" ")
+	
+end
+
+
+def is_vowel(char)
+	["a","e","i","o","u"].include?(char)
+end
+
+
+def translate_word(word)
+	if is_vowel word[0]
+		word + "ay"
+	else
+		substr = ""
+		n = 0
+
+		while !is_vowel word[n] or (word[n] == 'u' and word[n-1] == 'q')
+			substr += word[n]
+			n += 1
+			if substr[-2..-1] == 'qu'
+				break
+			end
+		end
+
+		word = word[n..-1] + substr + "ay"
+	end
+end
+
+
+def translate(sentence)
+	sentence.split.map { |word| translate_word word }.join(" ")
+end
