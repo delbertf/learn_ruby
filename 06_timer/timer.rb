5c05b241e580be82d7bb0a5fd49f82c792e310cd
Delbert F <7155672+delbertf@users.noreply.github.com>
2020-01-12 22:43:48 +0100

    fix test 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,25 @@
 class Timer
   #write your code here
+  attr_accessor :seconds, :hours, :minutes
+
+  def initialize
+  	@seconds = 0
+  	@hours = 0
+  	@minutes = 0
+  end
+
+  def time_string
+  	@hours.to_s.rjust(2,'0') + ":" + @minutes.to_s.rjust(2,'0') + ":" + @seconds.to_s.rjust(2,'0')
+  end
+
+  def seconds=(value)
+  	@minutes += value / 60
+  	@seconds += value % 60
+
+  	if @minutes / 60 > 0 
+  		@hours += @minutes / 60
+  		@minutes = @minutes % 60
+  	end
+  end
+
 end
